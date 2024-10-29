e1f868126d08f0e015655a18452d43ce9d2c6b93
jlopez1807 <146575920+jlopez1807@users.noreply.github.com>
2024-10-29 12:05:14 -0700

    Create Dashboard.php

---
+++ API_assignment/Visualization/PHP_files/Dashboard.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Adding headers to webpage -->
+<head>
+    <meta charset="utf-8">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <title>Electrical Engineering @ Sonoma State</title>
+    
+<!-- Styling with CSS -->
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+            background-color: rgba(0, 255, 0, 0.37);
+        }
+        h1, h2, h3 {
+            text-align: center;
+            color: rgba(0, 0, 0, 1);
+        }
+            iframe {
+            width: 100%;
+            max-width: 600px;
+            height: 370px;
+            border: none;
+            display: block;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<!-- Adding the graphs from Google spreadsheet -->
+<body>
+    <h1 style="font-size: 36px;">Electrical Engineering @ Sonoma State</h1>
+    <h2>Welcome to SSU IoT Class</h2>
+    <p>
+        <iframe 
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTCNArPXjqVSI1ElZIUA0N3XyCzlocVj4jV7WgT2Ew_N-M_GPbwj5-S59n1KsHqXiGt4ydHXom5X1CP/pubchart?oid=498517903&amp;format=interactive"
+            width="1000" 
+            height="470" 
+            frameborder="0" 
+            scrolling="no"></iframe>
+            
+            <iframe             
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTCNArPXjqVSI1ElZIUA0N3XyCzlocVj4jV7WgT2Ew_N-M_GPbwj5-S59n1KsHqXiGt4ydHXom5X1CP/pubchart?oid=1432866767&amp;format=interactive"
+            width="1000" 
+            height="470" 
+            frameborder="0" 
+            scrolling="no"></iframe> 
+    </p>
+<!-- Terminating header -->
+    <h3>Thank you for visiting this page</h3>
+</body>
+
+</html>
