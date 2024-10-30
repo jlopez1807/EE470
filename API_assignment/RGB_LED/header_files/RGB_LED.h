addc1059803e4b4068a274846ba89e3a13cb1343
jlopez1807 <146575920+jlopez1807@users.noreply.github.com>
2024-10-29 19:59:16 -0700

    Create RGB_LED.h

---
+++ API_assignment/RGB_LED/header_files/RGB_LED.h
@@ -0,0 +1,23 @@
+#ifndef RGB_LED_H
+#define RGB_LED_H
+
+// Adding libraries to the project
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include "wifi_cred.h"
+
+String rgbUrl = "https://ssu.jessielopez.net/PHP_Projects/tp2.txt"; // New URL for RGB values
+const unsigned long connectTimeoutMs = 30000;  // Timeout for 30 seconds
+
+
+// RGB LED pin configuration
+const int redPin = D1;   // Change as needed
+const int greenPin = D5; // Change as needed
+const int bluePin = D6;  // Change as needed
+
+// Function prototypes
+void controlRgbLed();
+
+#endif  // RGB_LED_H
